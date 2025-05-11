545ecf364e3c22f75aa169e23c311615950e93fc
Michael <46937389+LastSpot@users.noreply.github.com>
2025-05-10 23:36:37 -0400

    Add Supabase authentication functionality
    - Introduced Supabase client integration for user authentication.
    - Implemented login and signup functions with validation using Zod.
    - Updated package.json and pnpm-lock.yaml to include Supabase dependencies.
    - Enhanced auth.ts with user sign-in, sign-up, and sign-out capabilities.

--- cash-flow/src/actions/auth.ts
+++ cash-flow/src/actions/auth.ts
@@ -1,5 +1,21 @@
 "use server";
 
+import { createClient } from "@/lib/supabase/server";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
+import { z } from "zod";
+
+const loginSchema = z.object({
+  email: z.string().email(),
+  password: z.string().min(8),
+});
+
+const signupSchema = z.object({
+  email: z.string().email(),
+  password: z.string().min(8),
+  confirmPassword: z.string().min(8),
+});
+
 export async function verifyOTP(otp: string) {
   const adminSecret = process.env.ADMIN_SECRET;
   const employeeSecret = process.env.EMPLOYEE_SECRET;
@@ -19,4 +35,71 @@ export async function verifyOTP(otp: string) {
   return {
     success: false,
   };
+}
+
+export async function login(formData: FormData) {
+  const supabase = await createClient();
+  const validatedFields = loginSchema.safeParse({
+    email: formData.get("email"),
+    password: formData.get("password"),
+  });
+  if (!validatedFields.success) {
+    return "Missing fields. Failed to Login.";
+  }
+
+  const { email, password } = validatedFields.data;
+  const data = {
+    email: email as string,
+    password: password as string,
+  };
+
+  const { error } = await supabase.auth.signInWithPassword(data);
+  if (error) {
+    return error.message;
+  }
+
+  revalidatePath("/dashboard");
+  redirect("/dashboard");
+}
+
+export async function signup(formData: FormData) {
+  const supabase = await createClient();
+
+  const validatedFields = signupSchema.safeParse({
+    email: formData.get("email"),
+    password: formData.get("password"),
+    confirmPassword: formData.get("confirmPassword"),
+  });
+  if (!validatedFields.success) {
+    return "Missing fields. Failed to Signup.";
+  }
+
+  const { email, password, confirmPassword } = validatedFields.data;
+  if (password !== confirmPassword) {
+    return "Passwords do not match.";
+  }
+
+  const { error } = await supabase.auth.signUp({
+    email: email,
+    password: password,
+    options: {
+      data: {
+        display_name: email?.split("@")[0] as string,
+        avatar_url: "",
+      },
+    },
+  });
+  if (error) {
+    return error.message;
+  }
+
+  revalidatePath("/dashboard");
+  redirect("/dashboard");
+}
+
+export async function signout() {
+  const supabase = await createClient();
+  await supabase.auth.signOut();
+  revalidatePath("/", "layout");
+  redirect("/");
 }
